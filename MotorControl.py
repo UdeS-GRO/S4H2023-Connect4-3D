fa1e4b902903bf1de7791e516525bdd802004200
4lexandrb <bara1102@usherbrooke.ca>
2023-01-18 11:25:44 -0600

    angles to cartesian almost working

--- MotorControl.py
+++ MotorControl.py
@@ -3,25 +3,64 @@
 import struct
 #import numpy as np
 
-dataPack = struct.pack('iii', 0, 5, 10)
-data = struct.unpack('iii', dataPack)
+BicepLen = 150
+ForearmLen = 150
 
 def cart2cyl(cartX, cartY):
-    cylX = cartX
-    cylY = cartY
-    print([cylX, cylY])
-    return [cylX, cylY]
+    C2 = (cartX**2 + cartY**2 - BicepLen**2 - ForearmLen**2) / (2 * BicepLen * ForearmLen)
+    S2 = (1-C2) ** 0.5
+    theta = 5 #cos(C2)     ###cos-1 à impléementer!!!
+    phi = 10 #atan2((ForearmLen * S2 * cartX) + ((BicepLen + ForearmLen*C2)*cartY), ((BicepLen + ForearmLen*C2)*cartX)-(ForearmLen * S2 * cartY))
+    #print([theta, phi])
+    return [theta, phi]
 
-def Interpolation(posStart, posEnd):
-    print("interpolation")
-    return posStart + posEnd
+def deg2Servo(angleDeg):
+    angleServo = angleDeg * 4096 / 360
+    return angleServo
 
-def goDown(height):
+def Interpolation(posXStart, posYStart, posXEnd, posYEnd):
+    jointAngles = cart2cyl(posXStart, posYStart)
+    posx = posXStart
+    posy = posYStart
+    diffX = posXEnd - posXStart
+    diffY = posYEnd - posYStart
+
+    while posx != posXEnd and posy != posYEnd:
+        cart2cyl(posx, posy)
+        posx += (diffX/20)
+        posy += (diffY/20)
+        jointAngles = cart2cyl(posx, posy)
+        jointAngles[0] = deg2Servo(jointAngles[0])
+        jointAngles[1] = deg2Servo(jointAngles[1])
+        print(jointAngles)
+    return jointAngles
+
+def moveZ(height, direction):
+    #1 = down
+    #0 = up
+    #while encoderValue != range(height-50, height+50):
+    #    #motor.move(direction, speed, etc)
+    #    print(encoderValue)
+
     print("height = " + height)
     return
 
 #sends the 3 motor positions to the openCR board
 
-print(cart2cyl(data[0], data[1]))
+##### MAIN #####
+
+#receive from OpenCR card:
+    #encodervalue, motorShoulderposition, motorElbowposition
+
+#dataPack = struct.pack('iii', encodervalue, motorShoulderPosition, motorElbowPosition)
+#data = struct.unpack('iii', dataPack)
+
+#sends to OpenCR
+motorMvt = Interpolation(100, 50, 200, 150)
+motorShoulder = motorMvt[0]
+elbowShoulder = motorMvt[1]
+
+print(motorShoulder)
+print(elbowShoulder)
 
 print("hello")
