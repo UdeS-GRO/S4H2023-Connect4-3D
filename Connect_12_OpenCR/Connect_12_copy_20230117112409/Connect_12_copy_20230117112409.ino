ec1c5fee8ed8a8f0ae3bb5208c78cba7ffb1f7fb
4lexandrb <bara1102@usherbrooke.ca>
2023-01-25 20:56:18 -0600

    working with one motor

--- Connect_12_OpenCR/Connect_12_copy_20230117112409/Connect_12_copy_20230117112409.ino
+++ Connect_12_OpenCR/Connect_12_copy_20230117112409/Connect_12_copy_20230117112409.ino
@@ -2,15 +2,18 @@
 
 // For OpenCR, there is a DXL Power Enable pin, so you must initialize and control it.
 // Reference link : https://github.com/ROBOTIS-GIT/OpenCR/blob/master/arduino/opencr_arduino/opencr/libraries/DynamixelSDK/src/dynamixel_sdk/port_handler_arduino.cpp#L78
-#define motorShoulder_SERIAL   Serial3
-#define motorElbow_SERIAL   Serial2
-#define DEBUG_SERIAL Serial
-#define button1 34
-#define button2 35
+#if defined(ARDUINO_OpenCR)
+  #define motorShoulder_SERIAL   Serial3
+  #define motorElbow_SERIAL   Serial2
+  #define DEBUG_SERIAL Serial
+#endif
+
+#define BDPIN_PUSH_SW_1         34
+#define BDPIN_PUSH_SW_2         35
 
 const int motorShoulder_DIR_PIN = 84; // OpenCR Board's DIR PIN.
 const int motorElbow_DIR_PIN = 85; // OpenCR Board's DIR PIN.
-const uint8_t motorShoulder_ID = 1;
+const uint8_t motorShoulder_ID = 23;
 const uint8_t motorElbow_ID = 2;
 const float DXL_PROTOCOL_VERSION = 2.0;
 
@@ -20,9 +23,18 @@ Dynamixel2Arduino motorElbow(motorElbow_SERIAL, motorElbow_DIR_PIN);
 //This namespace is required to use Control table item names
 using namespace ControlTableItem;
 
+int max = 180;
+int min = 0;
+int ShoulderGoal = 0;
+
 void setup() {
   
   DEBUG_SERIAL.begin(115200);
+  while(!DEBUG_SERIAL);
+
+  pinMode(BDPIN_PUSH_SW_1, INPUT);
+  pinMode(BDPIN_PUSH_SW_2, INPUT);
+
 
   //motorShoulder Setup
   motorShoulder.begin(57600);
@@ -35,9 +47,9 @@ void setup() {
   motorShoulder.torqueOn(motorShoulder_ID);
 
   // Limit the maximum velocity in Position Control Mode. Use 0 for Max speed
-  motorShoulder.writeControlTableItem(PROFILE_VELOCITY, motorShoulder_ID, 30);
+  motorShoulder.writeControlTableItem(PROFILE_VELOCITY, motorShoulder_ID, 100);
 
-  //motorElbow Setup
+ /* //motorElbow Setup
   motorElbow.begin(57600);
   motorElbow.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
   motorElbow.ping(motorElbow_ID);
@@ -48,19 +60,23 @@ void setup() {
   motorElbow.torqueOn(motorElbow_ID);
 
   // Limit the maximum velocity in Position Control Mode. Use 0 for Max speed
-  motorElbow.writeControlTableItem(PROFILE_VELOCITY, motorElbow_ID, 30);
+  motorElbow.writeControlTableItem(PROFILE_VELOCITY, motorElbow_ID, 50);*/
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (digitalRead(button1)){
-    motorShoulder.setGoalPosition(motorShoulder_ID, 4000);
-    delay(500);
-    motorShoulder.setGoalPosition(motorShoulder_ID, 0);
+  if(digitalRead(BDPIN_PUSH_SW_1))
+  {
+    ShoulderGoal += 5;
+    if(ShoulderGoal >= 4095)
+      ShoulderGoal = 4095;
   }
-  if (digitalRead(button2)){
-    motorElbow.setGoalPosition(motorElbow_ID, 4000);
-    delay(500);
-    motorElbow.setGoalPosition(motorElbow_ID, 0);
+  else
+  {
+    ShoulderGoal -= 2;
+    if(ShoulderGoal <= 0)
+      ShoulderGoal = 0;
   }
+
+  motorShoulder.setGoalPosition(motorShoulder_ID, ShoulderGoal);
+  
 }
