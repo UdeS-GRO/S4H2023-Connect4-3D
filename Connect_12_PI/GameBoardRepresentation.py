e6599e101da05faf0f22a7f075606d01400374b6
Pierre-Luc Gagne <pierreluc.gagne10@gmail.com>
2023-03-08 13:32:18 -0500

    Vision working with AI pos

--- Connect_12_PI/GameBoardRepresentation.py
+++ Connect_12_PI/GameBoardRepresentation.py
@@ -4,14 +4,12 @@ import sys
 import cv2
 import pyzbar.pyzbar as pyzbar
 import time
-import os
 try:
     from MotorControl import MotorMove
 except:
     pass
 from PyQt5 import QtWidgets
 
-import os
 from streak_counter import streak_counter
 from UserInterface import userInterface
 
@@ -22,6 +20,7 @@ class gameboard(QtWidgets.QMainWindow):
     board = []
     LastList = [0 for _ in range(16)]
     var = 0
+    global cap
     
     def __init__(self):
         # Function used to display the user interface (UI) and let the user use inputs to move the robot
@@ -30,6 +29,11 @@ class gameboard(QtWidgets.QMainWindow):
         self.init_board()
         super().__init__()
         userInterface(self)    
+
+        # start camera
+        self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)        # Create a VideoCapture object, validate if your PC's cam is 1 or 0 for index
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)         # Set the focus distance
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)        # Set the focus distance
         return
     
     def init_board(self):
@@ -58,11 +62,9 @@ class gameboard(QtWidgets.QMainWindow):
         row = int(position_list[0])
         column = int(position_list[1])
         player_id = int(position_list[2])
-        limit_board = self.row_or_column_limit(row, column)
-        if limit_board == 1:
-            floor = self.determine_floor(row, column)
-            if floor != None and limit_board == 1:
-                self.board[floor - 1][row - 1][column - 1] = player_id
+        floor = self.determine_floor(row, column)
+        if floor != None:
+            self.board[floor - 1][row - 1][column - 1] = player_id
         return
 
     def delete_piece(self, row, column, floor):
@@ -70,18 +72,6 @@ class gameboard(QtWidgets.QMainWindow):
 
         self.board[floor - 1][row - 1][column - 1] = 0
         return
-
-    def row_or_column_limit(self, row, column):
-        # Returns an error if the player is trying to play out of the gameboard range limit. 
-        # Asks the player to play at another position. 
-
-        row = int(row)
-        column = int(column)
-        if row > 4 or column > 4:
-            print('This case is not reachable. Try again.')
-            self.add_piece(self.player_played())
-            return 0
-        return 1
 
     def determine_floor(self, row, column):
         # Calculate the actual floor of the piece, knowing the previous pieces played at this exact position. 
@@ -91,13 +81,7 @@ class gameboard(QtWidgets.QMainWindow):
         column = int(column)
         for i in range(1, 7):
             if self.board[i - 1][row - 1][column - 1] != 0:
-                floor = floor + 1   
-        if floor > 6:
-            print('This case is not reachable. Try again.')
-            self.add_piece(self.player_played())
-            return None
-        #else:
-            #print('floor value is : ', floor)               
+                floor = floor + 1               
         return floor
 
     def detect_win(self,position_list):
@@ -247,31 +231,7 @@ class gameboard(QtWidgets.QMainWindow):
                     streak = 0
         return False
 
-    def player_played(self):
-        # Actualize the gameboard status with the new inputs
-        ##player, column, row = self.take_picture()
-        ##vision_list = [str(row), str(column), str(player)]
-        ##print('vision list : ', vision_list)
-        ##self.add_piece(vision_list)
-
-        if self.push_button1.isChecked():
-            #user_input = self.line_edit1.text()    # Uncomment thoses lines to use the player's input
-            #entries = user_input.split()           # Comment thoses lines to use the vision input
-            #self.add_piece(entries)                # " "  
-            #self.line_edit1.clear()                # " "
-            self.push_button1.setChecked(False)
-
-        elif self.push_button2.isChecked():
-            #user_input = self.line_edit2.text()    # Uncomment thoses lines to use the player's input
-            #entries = user_input.split()           # Comment thoses lines to use the vision input
-            #self.add_piece(entries)                # " "
-            #self.line_edit2.clear()                # " "
-            self.push_button2.setChecked(False)
-        if(self.detect_win(entries)):
-                print("VICTORY!")
-        self.label.setText(self.print_board())
-        return vision_list
-
+    
     def submit_inputs_xyz(self):
         xPosition = self.line_edit3.text()
         yPosition = self.line_edit4.text()
@@ -321,7 +281,6 @@ class gameboard(QtWidgets.QMainWindow):
             self.toggle_button.setText("Manual Mode")
 
         return
-
 
     def submit_auto_startSequence(self):
         # Starts the state machine to make the robot play
@@ -409,80 +368,43 @@ class gameboard(QtWidgets.QMainWindow):
         MotorMove.moveCart(MotorMove, int(cartX1Position), int(cartY2Position), int(zPosition))
 
         if self.selected_btn and self.selected_floor:
-            self.button_played(self.selected_btn, self.selected_floor)
+            self.submit_robot_pos(self.selected_btn, self.selected_floor)
 
-    def button_played(self, btn, floor):
+    def submit_robot_pos(self, row, column, floor):
         # Returns the xyz coordinates of the position where the robot has to move to. 
         # The xyz values are hard coded based on experimental moves. The values may changes according to the robot environment. 
         # The reference position is A1 and then the other positinos are automatically generated. 
 
         height_constant = 300
         height_init = 0
-        gameboardPosition = [btn.text(), floor.text()]
-        print("gameboardposition : ", gameboardPosition)
-        return gameboardPosition
-
+        
         xA1Position = 1
         yA1Position = 1
         xgap = 5
         ygap = -5            # Use a negative value since the A1 position is at the top left
 
-        match btn.text():
-            case 'A1':
-                xPosition = xA1Position
-                yPosition = yA1Position
-            case 'A2':
-                xPosition = xA1Position
-                yPosition = yA1Position + ygap
-            case 'A3':
-                xPosition = xA1Position
-                yPosition = yA1Position + 2*ygap
-            case 'A4':
-                xPosition = xA1Position
-                yPosition = yA1Position + 3*ygap
+        if row == 1:
+            xPosition = xA1Position
+        elif row == 2:
+            xPosition = xA1Position + xgap
+        elif row == 3:
+            xPosition = xA1Position + 2*xgap
+        elif row == 4:
+            yPosition = yA1Position + 3*ygap
+        
+        if column == 1:
+            yPosition = yA1Position
+        elif column == 2:
+            yPosition = yA1Position + ygap
+        elif column == 3:
+            yPosition = yA1Position + 2*ygap
+        elif column == 4:
+            yPosition = yA1Position + 3*ygap
 
-            case 'B1':
-                xPosition = xA1Position + xgap
-                yPosition = yA1Position
-            case 'B2':
-                xPosition = xA1Position + xgap
-                yPosition = yA1Position + ygap
-            case 'B3':
-                xPosition = xA1Position + xgap
-                yPosition = yA1Position + 2*ygap
-            case 'B4':
-                xPosition = xA1Position + xgap
-                yPosition = yA1Position + 3*ygap
+        zPosition = height_init - floor* height_constant
 
-            case 'C1':
-                xPosition = xA1Position + 2*xgap 
-                yPosition = yA1Position 
-            case 'C2':
-                xPosition = xA1Position + 2*xgap 
-                yPosition = yA1Position + ygap
-            case 'C3':
-                xPosition = xA1Position + 2*xgap 
-                yPosition = yA1Position + 2*ygap
-            case 'C4':
-                xPosition = xA1Position + 2*xgap 
-                yPosition = yA1Position + 3*ygap
-            
-            case 'D1':
-                xPosition = xA1Position + 3*xgap 
-                yPosition = yA1Position 
-            case 'D2':
-                xPosition = xA1Position + 3*xgap
-                yPosition = yA1Position + ygap
-            case 'D3':
-                xPosition = xA1Position + 3*xgap
-                yPosition = yA1Position + 2*ygap
-            case 'D4':
-                xPosition = xA1Position + 3*xgap
-                yPosition = yA1Position + 3*ygap
-
-        zPosition = height_init - int((floor.text())[5]) * height_constant
-        #print("gameboardposition : ", gameboardPosition)
-        #print("x  ", xPosition, "    y  ", yPosition)
+        MotorMove.Zpos = zPosition
+        MotorMove.moveCart(MotorMove, int(xPosition), int(yPosition), int(zPosition))
         return xPosition, yPosition, zPosition
 
     def take_picture(self):
@@ -491,22 +413,18 @@ class gameboard(QtWidgets.QMainWindow):
 
         start_time = time.time()
         list = self.LastList[:]
-
-        cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)        # Create a VideoCapture object 
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)         # Set the focus distance
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)        # Set the focus distance
-
+     
         while list == self.LastList:
             i=0
-            ret, img = cap.read()                       # Capture a frame from the webcam
+            ret, img = self.cap.read()                       # Capture a frame from the webcam
+
+            center = (img.shape[1]//2, img.shape[0]//2) # Crop the image and divise it into 16 squares
+            size = (1080-300, 1080-300)
+            img = cv2.getRectSubPix(img, size, center)
             cv2.imshow("Webcam", img)                   # Show the frame
 
             if cv2.waitKey(1) & 0xFF == ord('q'):       # Exit the loop if the 'q' key is pressed
                 break
-
-            center = (img.shape[1]//2, img.shape[0]//2) # Crop the image and divise it into 16 squares
-            size = (1080, 1080)
-            img = cv2.getRectSubPix(img, size, center)
 
             height, width, _ = img.shape
             square_size = (height//4, width//4)
@@ -521,10 +439,6 @@ class gameboard(QtWidgets.QMainWindow):
                         data = qr_code.data.decode()    # Get QR code data
                         list[i]=(int(data))                
                     i+=1
-                    
-            os.system('cls' if os.name == 'nt' else 'clear')    # Print the game board
-            for i in range(0, 16, 4):
-                print(list[i:i+4])
             
             time.sleep(0.2)                             # Wait 0.2 seconds
         
@@ -533,21 +447,18 @@ class gameboard(QtWidgets.QMainWindow):
                 x = i%4+1
                 y = i//4+1
                 if (a < 47):
-                    Player = 'R'
+                    Player = 2
                 else:
-                    Player = 'U'
+                    Player = 4
 
         self.LastList = list
-        #print("--- %s seconds ---" % (time.time() - start_time))
-        cap.release()                                   # Release the VideoCapture object and Close all the windows
         cv2.destroyAllWindows()
+
         return Player, x, y
     
 if __name__ == "__main__":
-    #app = QApplication(sys.argv)
     gm = gameboard
     app = QtWidgets.QApplication(sys.argv)
     window = gameboard()
-    window.push_button2.clicked.connect(window.player_played)
     window.show()
     sys.exit(app.exec_())
