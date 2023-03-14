0be16172582596a99d31b4c452acec086b53c687
Edouard5618 <edouard5618@gmail.com>
2023-03-14 10:21:32 -0400

    Send 45 or 90 message for pick place

--- Connect_12_PI/GameBoardRepresentation.py
+++ Connect_12_PI/GameBoardRepresentation.py
@@ -237,54 +237,71 @@ class gameboard(QtWidgets.QMainWindow):
         if(row == 1 and column == 1):
             J1 = 1783
             J2 = 3523
+            PickPlace = 90
         elif (row == 1 and column == 2):
             J1 = 1082
             J2 = 3677
+            PickPlace = 90
         elif (row == 1 and column == 3):
             J1 = 3401
             J2 = 517
+            PickPlace = 90
         elif (row == 1 and column == 4):
             J1 = 2706
             J2 = 685
+            PickPlace = 90
         elif (row == 2 and column == 1):
             J1 = 3998
             J2 = 934
+            PickPlace = 90
         elif (row == 2 and column == 2):
             J1 = 3711
             J2 = 800
+            PickPlace = 90
         elif (row == 2 and column == 3):
             J1 = 3229
             J2 = 796
+            PickPlace = 90
         elif (row == 2 and column == 4):
             J1 = 2719
             J2 = 931
+            PickPlace = 90
         elif (row == 3 and column == 1):
             J1 = 3555
             J2 = 1250
+            PickPlace = 90
         elif (row == 3 and column == 2):
             J1 = 3307
             J2 = 1139
+            PickPlace = 90
         elif (row == 3 and column == 3):
             J1 = 2978
             J2 = 1155
+            PickPlace = 90
         elif (row == 3 and column == 4):
             J1 = 2567
             J2 = 1240
+            PickPlace = 90
         elif (row == 4 and column == 1):
             J1 = 2829
             J2 = 1897
+            PickPlace = 90
         elif (row == 4 and column == 2):
             J1 = 2828
             J2 = 1633
+            PickPlace = 90
         elif (row == 4 and column == 3):
             J1 = 2523
             J2 = 1660
+            PickPlace = 90
         elif (row == 4 and column == 4):
             J1 = 1987
             J2 = 1962
+            PickPlace = 90
         else:
             J1 = 0
             J2 = 0
+            PickPlace = 90
             print("Error: Invalid position")
         
         height_constant = -300
@@ -314,9 +331,11 @@ class gameboard(QtWidgets.QMainWindow):
             yPosition = yA1Position + 3*ygap
         '''
         zPosition = height_init + floor*height_constant
-        
-        MotorMove.mssg5 = "1"
-        print(MotorMove.mssg5)
+        if PickPlace == 90:
+            MotorMove.mssg5 = "1"
+        elif PickPlace == 45:
+            MotorMove.mssg5 = "0"
+    
         MotorMove.Zpos = zPosition
         #MotorMove.moveCart(MotorMove, xPosition, yPosition, zPosition)
         MotorMove.moveJoint(MotorMove, J1, J2)
