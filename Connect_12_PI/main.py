9c39b25dd3554830a05ad56751ee9f3f3b8c84b7
Pierre-Luc Gagne <sandrine9690@gmail.com>
2023-03-21 16:03:46 -0400

    Clean UI without the manuel mode

--- Connect_12_PI/main.py
+++ Connect_12_PI/main.py
@@ -19,23 +19,17 @@ def AI_played(self):
 
 
 def player_played(self):
-        # Actualize the gameboard status with the new inputs
-        player, column, row = gb.take_picture()
-        entries = [str(row), str(column), str(player)]
-        if gb.push_button1.isChecked():
-            #user_input = self.line_edit1.text()    # Uncomment thoses lines to use the player's input
-            #entries = user_input.split()           # Comment thoses lines to use the vision input
-            #self.add_piece(entries)                # " "  
-            #self.line_edit1.clear()                # " "
-            gb.push_button1.setChecked(False)
-        if(gb.detect_win(entries)):
-                MotorMove.sendVictory(MotorMove, 1)
-                print("VICTORY!")
-                gb.cap.release()                                   # Release the VideoCapture object and Close all the windows
-        gb.add_piece(entries)
-        AI_played(self)
-        gb.label.setText(gb.print_board())
-        return 
+    # Actualize the gameboard status with the new inputs
+    player, column, row = gb.take_picture()
+    entries = [str(row), str(column), str(player)]
+    if(gb.detect_win(entries)):
+            MotorMove.sendVictory(MotorMove, 1)
+            print("VICTORY!")
+            gb.cap.release()  # Release the VideoCapture object and Close all the windows
+    gb.add_piece(entries)
+    AI_played(self)
+    gb.label.setText(gb.print_board())
+    return 
 
 if __name__=="__main__":
     os.system('cls')
@@ -43,6 +37,6 @@ if __name__=="__main__":
     gb = gameboard()
     gb.board
     AI = AI(gb)
-    gb.push_button1.clicked.connect(player_played)
+    gb.submit_button4.clicked.connect(player_played)
     gb.show()
     sys.exit(app.exec_())
